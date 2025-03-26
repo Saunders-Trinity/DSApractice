102e66851ed5f7c534107dca634bc88e1298c05a
Trinity Saunders <148916740+Saunders-Trinity@users.noreply.github.com>
2025-03-25 21:37:41 -0400

    Create bst.h

---
+++ bst.h
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+// Node structure for a Binary Search Tree
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Function to create a new Node
+Node* createNode(int data)
+{
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+// Function to insert a node in the BST
+Node* insertNode(Node* root, int data)
+{
+    if (root == nullptr) { // If the tree is empty, return a
+                           // new node
+        return createNode(data);
+    }
+
+    // Otherwise, recur down the tree
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    }
+    else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+
+    // return the (unchanged) node pointer
+    return root;
+}
+
+// Function to do inorder traversal of BST
+void inorderTraversal(Node* root)
+{
+    if (root != nullptr) {
+        inorderTraversal(root->left);
+        cout << root->data << " ";
+        inorderTraversal(root->right);
+    }
+}
+
+// Function to search a given key in a given BST
+Node* searchNode(Node* root, int key)
+{
+    // Base Cases: root is null or key is present at root
+    if (root == nullptr || root->data == key) {
+        return root;
+    }
+// Key is greater than root's key
+    if (root->data < key) {
+        return searchNode(root->right, key);
+    }
+
+    // Key is smaller than root's key
+    return searchNode(root->left, key);
+}
+
+// Function to find the inorder successor
+Node* minValueNode(Node* node)
+{
+    Node* current = node;
+    // loop down to find the leftmost leaf
+    while (current && current->left != nullptr) {
+        current = current->left;
+    }
+    return current;
+}
+
+// Function to delete a node
+Node* deleteNode(Node* root, int data)
+{
+    if (root == nullptr)
+        return root;
+// If the data to be deleted is smaller than the root's
+    // data, then it lies in the left subtree
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    }
+    // If the data to be deleted is greater than the root's
+    // data, then it lies in the right subtree
+    else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    }
+    // if data is same as root's data, then This is the node
+    // to be deleted
+    else {
+        // node with only one child or no child
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // node with two children: Get the inorder successor
+        // (smallest in the right subtree)
+        Node* temp = minValueNode(root->right);
+// Copy the inorder successor's content to this node
+        root->data = temp->data;
+
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
